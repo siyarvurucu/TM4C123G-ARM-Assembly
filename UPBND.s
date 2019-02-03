a6a2575ea73f55cbab3d09285caa934cdbf60ab7
Şiyar Vurucu <siyar.r.vurucu@gmail.com>
2019-02-04 02:40:28 +0300

    Update UPBND.s

--- UPBND.s
+++ UPBND.s
@@ -1,4 +1,4 @@
-			; boundary update for number guessing. Pass N for 2^N upperbound, in R3
+; boundary update for number guessing. Pass N for 2^N upperbound, in R3
 			
 			AREA    	routines, READONLY, CODE, ALIGN=2
 			THUMB
@@ -9,26 +9,26 @@
 
 
 UPBND		
-			SUB			R3, #0x01       ; n = n-1
-			MOV			R2, #0x01		; 2^0 , to be shifted accordingly
-			LSL			R2 , R2, R3     ; 2^R3
-			ADD			R4, R2		    ; guessed number generated in R4
-			BL 			CONVRT			; converted to decimal ascii
-			BL			OutStr			; printed to screen
-			BL			InChar			; waiting for input
+			SUB		R3, #0x01      	        ; n = n-1
+			MOV		R2, #0x01		; 2^0 , to be shifted accordingly
+			LSL		R2 , R2, R3     	; 2^R3
+			ADD		R4, R2		    	; guessed number generated in R4
+			BL 		CONVRT			; converted to decimal ascii
+			BL		OutStr			; printed to screen
+			BL		InChar			; waiting for input
 			CMP 		R5, #0x043		; C
-			BEQ			found
-			CMP			R5, #0x055		; Up
-			BEQ			UPBND			;
+			BEQ		found
+			CMP		R5, #0x055		; Up
+			BEQ		UPBND			;
 			CMP 		R5, #0x044		; Down
-			ITT			EQ	
+			ITT		EQ	
 			SUBEQ		R4, R4, R2		;
-			BEQ			UPBND	
-			B			error
-found									; found state
-			MOV			R1, R4
-			BL			OutStr
-			BX			LR
-error		B			error
+			BEQ		UPBND	
+			B		error
+found								; found state
+			MOV		R1, R4
+			BL		OutStr
+			BX		LR
+error			B		error
 			
 			END
