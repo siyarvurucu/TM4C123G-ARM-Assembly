805199ada67afbd144df9b6b0e227095265c5568
Şiyar Vurucu <siyar.r.vurucu@gmail.com>
2019-02-04 02:41:18 +0300

    Update UserInput.s

--- UserInput.s
+++ UserInput.s
@@ -1,23 +1,23 @@
-;LABEL		DIRECTIVE	VALUE		COMMENT
+;LABEL			DIRECTIVE	VALUE			COMMENT
 			EXTERN 	 	InChar
 			EXTERN 		OutChar
 			EXTERN		OutStr
 			EXPORT 		__main
 			EXTERN 		CONVRT
-NUM  	    EQU     	0x20000600
+NUM  	    		EQU     	0x20000600
 			AREA 		main, READONLY, CODE
 				
 __main	
 			LDR 		R2, =0x20000400
-			MOV			R1, #0x04  		; counter, one ASCII character takes 1 byte
-loop		BL			InChar     		; waits for the input
-			STRB		R5,[R2], #-0x01 ; stores input to the address in R2
+			MOV		R1, #0x04  		; counter, one ASCII character takes 1 byte
+loop			BL		InChar     		; waits for the input
+			STRB		R5,[R2], #-0x01 	; stores input to the address in R2
 			SUBS		R1, #0x01		; decrement counter
 			BNE 		loop
 			ADD 		R2, #0x01		; correct the address
 			LDRH		R4, [R2]		; load half word to R4
-			LDR			R5,=NUM			; R5 address to converting loc.
-			BL          CONVRT			; make convert
-			BL			OutStr			; print it
-Forever		B 			Forever
+			LDR		R5,=NUM			; R5 address to converting loc.
+			BL              CONVRT			; make convert
+			BL		OutStr			; print it
+Forever			B 		Forever
 			END
