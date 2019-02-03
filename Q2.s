9172686b8eee94199debc53f9f04214e67147114
Şiyar Vurucu <siyar.r.vurucu@gmail.com>
2019-02-04 02:38:52 +0300

    Update Q2.s

--- Q2.s
+++ Q2.s
@@ -2,15 +2,15 @@
 			EXPORT 		__main
 			EXTERN 		CONVRT
 			EXTERN 	 	InChar
-NUM			EQU			0x20000500
+NUM			EQU		0x20000500
 			AREA 		main, READONLY, CODE
 				
-__main		BL			InChar
-			LDR			R5,=NUM
-			LDR			R4,[R5]
-			LDR			R5,=0x20000600
-			BL			CONVRT
-			BL			OutStr
+__main			BL		InChar
+			LDR		R5,=NUM
+			LDR		R4,[R5]
+			LDR		R5,=0x20000600
+			BL		CONVRT
+			BL		OutStr
 			
 			END
 			
