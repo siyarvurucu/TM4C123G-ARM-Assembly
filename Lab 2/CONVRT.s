bf64e4eedad6d81aa8ada9473b3716f08be62656
Şiyar Vurucu <siyar.r.vurucu@gmail.com>
2019-02-04 18:48:04 +0300

    Add files via upload

---
+++ Lab 2/CONVRT.s
@@ -0,0 +1,25 @@
+			; HEX TO DEC converter. Pass the hex in R4, location in R5
+			
+			AREA    	routines, READONLY, CODE, ALIGN=2
+			THUMB
+			EXPORT 		CONVRT	;
+CONVRT 		
+			PUSH	   {R0-R4}
+			MOV			R0, #10              ; Decimal
+			MOV			R3, #0x04            ; 0x04 to be added at the end of string for OutStr
+			STRB		R3, [R5], #-1		 ;
+			
+Loop		UDIV 		R1, R4, R0			 ;  We divide our number by ten and save quotient to R1
+			MLS 		R2, R1, R0, R4  	 ; 	From quotient, we calculate the remainder, save it to R2
+			ADD			R2, #0x30			 ;  R2 is our decimal digit, we add 0x30 to make it ASCII char. for digit
+			STRB   		R2, [R5], #-1		 ;  Storing it to R5, decrementing R5. OutStr reads by incrementing so our LSB will be at last register.
+			MOV 		R4, R1				 ;  Now quotient replaces our original number.
+			CMP 		R1, #0				 ;  If it is zero, that means we just saved the last digit.
+			BNE			Loop
+			ADD			R5, #1				 ;  Correcting the address in R5
+			POP		   {R0-R4}
+			BX			LR
+			
+			
+			ALIGN                           ;
+			END                             ; end of file
---
+++ Lab 2/data.s
@@ -0,0 +1,56 @@
+GPIO_PORTB_DATA 	EQU 	0x400053FC ; data address to all pins 2 
+GPIO_PORTB_DIR 		EQU 	0x40005400 
+GPIO_PORTB_AFSEL 	EQU 	0x40005420 
+GPIO_PORTB_DEN 		EQU 	0x4000551C 
+IOB 				EQU 	0xF0 	
+SYSCTL_RCGCGPIO 	EQU 	0x400FE608 
+PBinput				EQU		0x4000503C
+PBoutput			EQU		0x400053C0
+
+	
+					AREA   	main, READONLY, CODE, ALIGN=2 
+					THUMB 
+					EXPORT 	__main
+					EXTERN	DELAY100
+					EXTERN	OutChar
+__main						
+					LDR 	R1, =SYSCTL_RCGCGPIO 
+										
+					LDR 	R0, [R1] 
+					ORR 	R0, R0, #0x12 
+					STR 	R0, [R1] 
+					NOP 
+					NOP 
+					NOP 							; let GPIO clock stabilize 
+									
+					LDR	 	R1, =GPIO_PORTB_DIR 	; config . of port B starts 
+					LDR 	R0, [R1] 
+					BIC 	R0, #0xFF 				; R0 = 0
+					ORR 	R0, #IOB 				; or with 1111000
+					STR 	R0, [R1] 
+					LDR 	R1, =GPIO_PORTB_AFSEL   ;make pin functions controlled by gpio registers
+					LDR 	R0, [R1] 				
+					BIC 	R0, #0xFF 			
+					STR 	R0, [R1] 				;
+					LDR 	R1, =GPIO_PORTB_DEN 
+					LDR 	R0, [R1] 
+					ORR 	R0, #0xFF 
+					STR 	R0, [R1] 				; config . of port B ends
+				
+					LDR		R1, =GPIO_PORTB_DATA
+					LDR		R5, =0x0041
+loop				BL		OutChar
+					LDR		R0, [R1]
+					BL		DELAY100
+					LSL		R0,#0x04
+					STR		R0, [R1]
+					LDR		R2,=0x032
+FIVESEC				BL 		DELAY100
+					SUBS	R2,#0x01
+					CMP 	R2,#0x00
+					BNE		FIVESEC
+					B		loop			
+					END
+		
+
+							
