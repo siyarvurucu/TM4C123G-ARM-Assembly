77a63a1e719c329c727fcfab6ce958adefbf6cdf
Şiyar Vurucu <siyar.r.vurucu@gmail.com>
2019-02-05 02:27:12 +0300

    Update ADC0_set.s

--- Lab 5/ADC0_set.s
+++ Lab 5/ADC0_set.s
@@ -1,14 +1,16 @@
-ADCACTSS	EQU		0x40038000
-ADCEMUX		EQU		0x40038014
-ADCSSCTL3	EQU		0x400380A4
-ADCPC		EQU   	0x40038FC4
-RCGCADC		EQU		0x400FE638
-			AREA 	routines, CODE, READONLY
+# Initialization of ADC0
+
+ADCACTSS		EQU		0x40038000
+ADCEMUX			EQU		0x40038014
+ADCSSCTL3		EQU		0x400380A4
+ADCPC			EQU   		0x40038FC4
+RCGCADC			EQU		0x400FE638
+			AREA 		routines, CODE, READONLY
 			THUMB
-			EXPORT 	ADC0_set
+			EXPORT 		ADC0_set
 
 ADC0_set	
-			PUSH	{R0-R1}
+			PUSH		{R0-R1}
 			
 			LDR		R1,=RCGCADC
 			MOV		R0,#0X01
@@ -24,23 +26,23 @@ ADC0_set
 			NOP 
 			NOP
 			
-			LDR		R1,=ADCACTSS ; disable sequencer 3
+			LDR		R1,=ADCACTSS  	 ; disable sequencer 3
 			MOV		R0,#0X0
 			STR		R0,[R1]
 			
 			LDR		R1,=ADCEMUX
 			MOV		R0,#0X0
-			STR		R0,[R1]	     ; clear bits 15:12
+			STR		R0,[R1]	         ; clear bits 15:12
 
 			LDR		R1,=ADCSSCTL3
 			MOV		R0,#0x06	 ; set IE0, END0
 			STR		R0,[R1]
 			
 			LDR		R1,=ADCPC
-			MOV 	R0,#0X01
+			MOV 		R0,#0X01
 			STR		R0,[R1]		 ; select 125 ksps
 			
-			LDR		R1,=ADCACTSS ; enable sequencer 3
+			LDR		R1,=ADCACTSS 	  ; enable sequencer 3
 			MOV		R0,#0x08
 			STR		R0,[R1]
 			
