e84d3f844459330f0080e15f0d82890a506f74f6
Şiyar Vurucu <siyar.r.vurucu@gmail.com>
2019-02-05 02:30:47 +0300

    Update part_three.s

--- Lab 5/part_three.s
+++ Lab 5/part_three.s
@@ -5,11 +5,11 @@
 			EXTERN	CONVRT_exp5
 			EXTERN	part_two
 				
-part_three	PUSH	{R2,R3,R4,LR}
+part_three		PUSH	{R2,R3,R4,LR}
 			SUB		R3,R4,R10
-			SUBS 	R2,R3,R9
+			SUBS 		R2,R3,R9
 			BLO		lower
-			ADDS	R2,R3,R9
+			ADDS		R2,R3,R9
 			BHI		lower
 			BL		part_two
 			ADD		R10,R4,#0X0
