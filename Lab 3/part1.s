eeca56e3ba72e74a81be763d801012af6bf41e0a
Şiyar Vurucu <siyar.r.vurucu@gmail.com>
2019-02-05 01:18:32 +0300

    Update part1.s

--- Lab 3/part1.s
+++ Lab 3/part1.s
@@ -1,27 +1,27 @@
 
-;LABEL DIRECTIVE VALUE COMMENT
+;LABEL 			DIRECTIVE 	VALUE 			COMMENT
 			AREA main , CODE, READONLY
 			THUMB
-			EXPORT 	rotateright
-			EXTERN  DELAYSome	
-			EXTERN	rotateleft
-GPIO_PORTB_DATA 	EQU 	0x400053FC
+			EXPORT 		rotateright
+			EXTERN  	DELAYSome	
+			EXTERN		rotateleft
+GPIO_PORTB_DATA 	EQU 		0x400053FC
 rotateright 					
-			LDR 	R1, =GPIO_PORTB_DATA
+			LDR 		R1, =GPIO_PORTB_DATA
 			
-loop		CMP 	R11,#0x00			; left button pressed?
+loop		CMP 	R11,#0x00				; left button pressed?
 			BEQ		rotateleft
-			MOV  	R0, #0x01			; first input 0001
-			STR  	R0, [R1]
+			MOV  		R0, #0x01			; first input 0001
+			STR  		R0, [R1]
 			BL		DELAYSome			; wait after first step
-			LSL 	R0,#0x01			; shift to second input
-			STR  	R0, [R1]			; 0010
+			LSL 		R0,#0x01			; shift to second input
+			STR  		R0, [R1]			; 0010
 			BL		DELAYSome			
-			LSL 	R0,#0x01			
-			STR  	R0, [R1]			; 0100
+			LSL 		R0,#0x01			
+			STR  		R0, [R1]			; 0100
 			BL		DELAYSome
-			LSL 	R0,#0x01
-			STR  	R0, [R1]			; 1000
+			LSL 		R0,#0x01
+			STR  		R0, [R1]			; 1000
 			BL		DELAYSome
 			B 		loop
 			END
