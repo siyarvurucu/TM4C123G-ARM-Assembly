5b6b554e2da67a1f06be8952110cb309a431b223
Şiyar Vurucu <siyar.r.vurucu@gmail.com>
2019-02-05 00:57:44 +0300

    Update chartodec.s

--- Lab 2/chartodec.s
+++ Lab 2/chartodec.s
@@ -1,39 +1,41 @@
-;LABEL		DIRECTIVE	VALUE		COMMENT
-			EXTERN 	 	InChar
-			EXPORT 		chartodec
-			EXTERN 		CONVRT
-NUM  	    EQU     	0x20000600
-			AREA 		routines, READONLY, CODE
-				
-				
-chartodec	PUSH		{LR}
-			PUSH		{R0-R4}
-			MOV			R3,#0x0A
-			MOV			R2,#0x00
-			MOV			R1,#0x00	;counter
-			LDR			R0,=NUM
-loop		BL			InChar
-			CMP 		R5,#0x020
-			ITT			EQ
-			MOVEQ		R5,#0x00
-			BEQ 		go
-			SUB			R5,#0x030
-			STRB		R5,[R0],#-0x01
-			ADD 		R1,#0x01
-			B			loop
-			
-go 			
-			LDRB		R2,[R0,R1]
-			SUBS		R1,#0x01
-			BEQ			fin
-			ADD			R5,R2
-			MUL			R5,R3
-			B			go
-fin			
-			ADD			R5, R2
+; Reads ascii from UART, converts to hex number, stored in register R5
 
-			POP			{R0-R4}
-			POP			{LR}
-			BX 			LR
+;LABEL		DIRECTIVE	VALUE		COMMENT
+		EXTERN 	 	InChar
+		EXPORT 		asciitonumber
+		EXTERN 		CONVRT
+NUM_Addr    	EQU     	0x20000600
+		AREA 		routines, READONLY, CODE
+				
+				
+asciitonumber	PUSH		{LR}
+		PUSH		{R0-R4}
+		MOV		R3,#0x0A
+		MOV		R2,#0x00
+		MOV		R1,#0x00	;counter
+		LDR		R0,=NUM_Addr
+loop		BL		InChar
+		CMP 		R5,#0x020	; If input char is space, sequence ended, start convert
+		ITT		EQ
+		MOVEQ		R5,#0x00
+		BEQ 		convert
+		SUB		R5,#0x030
+		STRB		R5,[R0],#-0x01
+		ADD 		R1,#0x01
+		B		loop
 			
-			END
+convert 			
+		LDRB		R2,[R0,R1]
+		SUBS		R1,#0x01
+		BEQ		fin
+		ADD		R5,R2
+		MUL		R5,R3
+		B		go
+fin			
+		ADD		R5, R2
+
+		POP		{R0-R4}
+		POP		{LR}
+		BX 		LR
+			
+		END
