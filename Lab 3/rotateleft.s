0c77385f5a157b030c79094878f3a79979f9c776
Şiyar Vurucu <siyar.r.vurucu@gmail.com>
2019-02-05 01:24:58 +0300

    Update rotateleft.s

--- Lab 3/rotateleft.s
+++ Lab 3/rotateleft.s
@@ -8,19 +8,19 @@ GPIO_PORTB_DATA 	EQU 	0x400053FC
 rotateleft 	
 			LDR 	R1, =GPIO_PORTB_DATA
 
-loop		CMP 	R11,#0x01
-			BEQ		rotateright
+loop			CMP 	R11,#0x01
+			BEQ	rotateright
 			MOV  	R0, #0x08	; 1000
 			STR  	R0, [R1]
-			BL		DELAYSome
+			BL	DELAYSome
 			LSR 	R0,#0x01	; 0100 
 			STR  	R0, [R1]
-			BL		DELAYSome
+			BL	DELAYSome
 			LSR 	R0,#0x01	; 0010
 			STR  	R0, [R1]
-			BL		DELAYSome
+			BL	DELAYSome
 			LSR 	R0,#0x01	; 0001
 			STR  	R0, [R1]
-			BL		DELAYSome
-			B 		loop
+			BL	DELAYSome
+			B 	loop
 			END
