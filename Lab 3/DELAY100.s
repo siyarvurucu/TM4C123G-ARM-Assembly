f6c8a21a88c225b0239ed3fbc8274cff1bea4c56
Şiyar Vurucu <siyar.r.vurucu@gmail.com>
2019-02-05 01:13:31 +0300

    Update DELAY100.s

--- Lab 3/DELAY100.s
+++ Lab 3/DELAY100.s
@@ -1,13 +1,13 @@
 ;LABEL		DIRECTIVE	VALUE			COMMENT
-			EXPORT 		DELAY100
-			AREA 		routines, READONLY, CODE
+		EXPORT 		DELAY100
+		AREA 		routines, READONLY, CODE
 				
 DELAY100	PUSH 		{R1}
-			LDR			R1,=410858
+		LDR		R1,=410858
 loop		SUBS		R1,#0x01
-			CMP 		R1,#0x00
-			BNE			loop
-			POP			{R1}
-			BX			LR
+		CMP 		R1,#0x00
+		BNE		loop
+		POP		{R1}
+		BX		LR
 					
-			END
+		END
